7ed62125b2ebc1e8a637b3dc3359e5c6063a3fe1
Alex <s1129351@student.windesheim.nl>
2019-12-06 09:09:14 +0100

    StudentEditViewmodel

---
+++ PreagusFietsen/PreagusFietsen/ViewModel/StudentEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreagusFietsen.ViewModel
+{
+    class StudentEditViewModel
+    {
+        public List<Student> Students { get; set; };
+
+        public StudentEditViewModel()
+        {
+            Students = new List<Student>
+            {
+
+            }
+        }
+    }
+}
