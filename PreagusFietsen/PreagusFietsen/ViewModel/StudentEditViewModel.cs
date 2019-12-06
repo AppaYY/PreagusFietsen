ee24273bead3e8a77d4be6f0cd7e54d8880c9109
Alex <s1129351@student.windesheim.nl>
2019-12-06 09:34:07 +0100

    EditViewModel test

--- PreagusFietsen/PreagusFietsen/ViewModel/StudentEditViewModel.cs
+++ PreagusFietsen/PreagusFietsen/ViewModel/StudentEditViewModel.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PreagusFietsen.Model;
 
 namespace PreagusFietsen.ViewModel
 {
     class StudentEditViewModel
     {
-        public List<Student> Students { get; set; };
+        public List<Bikes> BikeList { get; set; }
 
         public StudentEditViewModel()
         {
-            Students = new List<Student>
+            BikeList = new List<Bikes>
             {
-
-            }
+                new Bikes()
+                {
+                    ID = 1,
+                    Type = "testType",
+                    Gender = "male",
+                }
+            };
         }
     }
 }
