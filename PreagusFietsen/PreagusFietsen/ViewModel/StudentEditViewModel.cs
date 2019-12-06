1f7d1f97f1e31e82f3c82c96111d5185971bc4d5
Caitlin <Caitlincoopmans@gmail.com>
2019-12-06 10:25:43 +0100

    Here are some bikes

--- PreagusFietsen/PreagusFietsen/ViewModel/StudentEditViewModel.cs
+++ PreagusFietsen/PreagusFietsen/ViewModel/StudentEditViewModel.cs
@@ -18,8 +18,43 @@ namespace PreagusFietsen.ViewModel
                 new Bikes()
                 {
                     ID = 1,
-                    Type = "testType",
+                    Type = "Schindelhauer Lotte",
+                    Gender = "female",
+                    Size = "large",
+                    Weight = 11.0,
+                    Speed = 11,
+                    HourRate = 0.2,
+                    DailyRate = 2,
+                },
+                new Bikes(){
+                    ID = 2,
+                    Type = "Whippet British Folding Bicycle",
                     Gender = "male",
+                    Size = "small",
+                    Weight = 11.5,
+                    Speed = 9,
+                    HourRate = 0.4,
+                    DailyRate = 4,
+                },
+                new Bikes(){
+                    ID = 3,
+                    Type = "Budnitz Alpha Belt Drive Commuter Bicycle",
+                    Gender = "male",
+                    Size = "medium",
+                    Weight = 11.5,
+                    Speed = 8,
+                    HourRate = 0.4,
+                    DailyRate = 4,
+                },
+                new Bikes(){
+                    ID = 4,
+                    Type = "?",
+                    Gender = "male",
+                    Size = "small",
+                    Weight = 11.5,
+                    Speed = 9,
+                    HourRate = 0.4,
+                    DailyRate = 4,
                 }
             };
         }
