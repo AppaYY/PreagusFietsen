82d3d88d9f2da9f4b36cb9c8673ea66dea7d3dfd
LisaGroen <lgroen@roc-dev.com>
2019-12-06 09:05:29 +0100

    Models gemaakt

---
+++ PreagusFietsen/PreagusFietsen/Model/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreagusFietsen.Model
+{
+    class Store { 
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int MaxCapacity { get; set; }
+    }
+}
