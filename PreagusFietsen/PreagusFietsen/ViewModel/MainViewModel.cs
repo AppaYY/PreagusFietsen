2fa14a3821711f3af0f656cc1230e399d67fdcfa
Alex <s1129351@student.windesheim.nl>
2019-12-20 12:11:01 +0100

    Fixed Bikes list

--- PreagusFietsen/PreagusFietsen/ViewModel/MainViewModel.cs
+++ PreagusFietsen/PreagusFietsen/ViewModel/MainViewModel.cs
@@ -8,6 +8,7 @@ namespace PreagusFietsen.ViewModel
     {
         public ObservableCollection<Store> Stores { get; set; }
         public ObservableCollection<Bike> Bikes { get; set; }
+        public Bike SelectedBike{ get; set; }
         public Store SelectedStore{ get; set; }
         public MainViewModel()
         {
@@ -19,50 +20,51 @@ namespace PreagusFietsen.ViewModel
                     Address = "1234AB 4",
                     City = "Almere",
                     MaxCapacity = 1334,
-                    Bikes = new ObservableCollection<Bike>
-                    {
-                        new Bike()
-                        {
-                            ID = 1,
-                            Type = "Schindelhauer Lotte",
-                            Gender = "female",
-                            Size = "large",
-                            WeightKG = 11.0,
-                            Speed = 11,
-                            HourRateEUR = 0.6,
-                            DailyRateEUR = 6,
-                        },
-                        new Bike(){
-                            ID = 2,
-                            Type = "Whippet British Folding Bicycle",
-                            Gender = "male",
-                            Size = "small",
-                            WeightKG = 11.5,
-                            Speed = 9,
-                            HourRateEUR = 0.4,
-                            DailyRateEUR = 4,
-                        },
-                        new Bike(){
-                            ID = 3,
-                            Type = "Budnitz Alpha Belt Drive Commuter Bicycle",
-                            Gender = "male",
-                            Size = "medium",
-                            WeightKG = 11.5,
-                            Speed = 8,
-                            HourRateEUR = 0.2,
-                            DailyRateEUR = 2,
-                        },
-                        new Bike(){
-                            ID = 4,
-                            Type = "Mokumono Delta Sportive Commuter Bike",
-                            Gender = "male",
-                            Size = "medium",
-                            WeightKG = 12.0,
-                            Speed = 11,
-                            HourRateEUR = 0.6,
-                            DailyRateEUR = 6,
-                        }
-                    }
+                }
+            };
+
+            Bikes = new ObservableCollection<Bike>
+            {
+                new Bike()
+                {
+                    ID = 1,
+                    Type = "Schindelhauer Lotte",
+                    Gender = "female",
+                    Size = "large",
+                    WeightKG = 11.0,
+                    Speed = 11,
+                    HourRateEUR = 0.6,
+                    DailyRateEUR = 6,
+                },
+                new Bike(){
+                    ID = 2,
+                    Type = "Whippet British Folding Bicycle",
+                    Gender = "male",
+                    Size = "small",
+                    WeightKG = 11.5,
+                    Speed = 9,
+                    HourRateEUR = 0.4,
+                    DailyRateEUR = 4,
+                },
+                new Bike(){
+                    ID = 3,
+                    Type = "Budnitz Alpha Belt Drive Commuter Bicycle",
+                    Gender = "male",
+                    Size = "medium",
+                    WeightKG = 11.5,
+                    Speed = 8,
+                    HourRateEUR = 0.2,
+                    DailyRateEUR = 2,
+                },
+                new Bike(){
+                    ID = 4,
+                    Type = "Mokumono Delta Sportive Commuter Bike",
+                    Gender = "male",
+                    Size = "medium",
+                    WeightKG = 12.0,
+                    Speed = 11,
+                    HourRateEUR = 0.6,
+                    DailyRateEUR = 6,
                 }
             };
         }
