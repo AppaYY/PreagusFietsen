83e621350df6e8e07db346fa3ba91b0b7999aa6c
Caitlin <Caitlincoopmans@gmail.com>
2019-12-20 12:10:09 +0100

    Functies toegevoegd

--- PreagusFietsen/PreagusFietsen/ViewModel/BikeEditViewModel.cs
+++ PreagusFietsen/PreagusFietsen/ViewModel/BikeEditViewModel.cs
@@ -1,15 +1,48 @@
 ﻿using System.Collections.ObjectModel;
 using PreagusFietsen.Model;
+using System.Windows;
 
 namespace PreagusFietsen.ViewModel
 {
     class BikeEditViewModel
     {
         public ObservableCollection<Bike> Bikes { get; set; }
-
+        public Bike SelectedBike { get; set; }
+        public RelayCommand ChangeNameClick { get; set; }
+        public RelayCommand DeleteClick { get; set; }
         public BikeEditViewModel(ObservableCollection<Bike> bikes)
         {
             Bikes = bikes; // set the property Courses (that is bound to the view) to be the collection we get passed from the other View
-        }   
+            ChangeNameClick = new RelayCommand(ChangeName);
+            DeleteClick = new RelayCommand(DeleteStudent)
+        }
+
+
+        public void ChangeName(object a)
+        {
+            if (SelectedBike != null)
+            {
+                SelectedBike.Type = "CHANGED TYPE";
+            }
+            else
+            {
+                MessageBox.Show("Please select a store first");
+            }
+        }
+
+        public void DeleteBike(object a)
+        {
+            if (SelectedBike != null)
+            {
+                Bikes.Remove(SelectedBike);
+            }
+            else
+            {
+                MessageBox.Show("Please select a student first");
+            }
+        }
+
     }
-}
+
+
+}  
