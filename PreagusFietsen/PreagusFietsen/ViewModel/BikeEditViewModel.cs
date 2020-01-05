b26de77124233a7a72dfcee1dc03f13889a53858
Alex <s1129351@student.windesheim.nl>
2020-01-05 21:57:10 +0100

    -Bike delete function -Bike add function

--- PreagusFietsen/PreagusFietsen/ViewModel/BikeEditViewModel.cs
+++ PreagusFietsen/PreagusFietsen/ViewModel/BikeEditViewModel.cs
@@ -10,15 +10,23 @@ namespace PreagusFietsen.ViewModel
         public Bike SelectedBike { get; set; }
         public RelayCommand ChangeNameClick { get; set; }
         public RelayCommand DeleteClick { get; set; }
-        public RelayCommand EditClick { get; set; }
+        public RelayCommand AddClick { get; set; }
         public BikeEditViewModel(ObservableCollection<Bike> bikes)
         {
             Bikes = bikes; // set the property Courses (that is bound to the view) to be the collection we get passed from the other View
             ChangeNameClick = new RelayCommand(ChangeName);
             DeleteClick = new RelayCommand(DeleteBike);
+            AddClick = new RelayCommand(AddBike);
         }
 
+        // ADD BIKE VALUES
         public string bikeType { get; set; }
+        public string bikeGender { get; set; }
+        public string bikeSize { get; set; }
+        public double bikeWeight { get; set; }
+        public int bikeSpeed { get; set; }
+        public double bikeHourRate { get; set; }
+        public int bikeDailyRate { get; set; }
 
         public void ChangeName(object a)
         {
@@ -42,6 +50,22 @@ namespace PreagusFietsen.ViewModel
             {
                 MessageBox.Show("Please select a bike please");
             }
+        }
+        
+        public void AddBike(object a)
+        {
+            var data = new Bike
+            {
+                Type = bikeType,
+                Gender = bikeGender,
+                Size = bikeSize,
+                WeightKG = bikeWeight,
+                Speed = bikeSpeed,
+                HourRateEUR = bikeHourRate,
+                DailyRateEUR = bikeDailyRate,
+            };
+
+            Bikes.Add(data);
         }
     }
 
