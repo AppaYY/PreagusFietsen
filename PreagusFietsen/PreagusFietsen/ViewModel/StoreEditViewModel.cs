19929841152821efdb2c41f8ff64c38eedd7c4a7
Caitlin <Caitlincoopmans@gmail.com>
2020-01-10 12:22:56 +0100

    Edit added

--- PreagusFietsen/PreagusFietsen/ViewModel/StoreEditViewModel.cs
+++ PreagusFietsen/PreagusFietsen/ViewModel/StoreEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using PreagusFietsen.Model;
 
 namespace PreagusFietsen.ViewModel
@@ -7,14 +8,14 @@ namespace PreagusFietsen.ViewModel
     {
         public ObservableCollection<Store> Stores { get; set; }
         public Store SelectedStore { get; set; }
-        //public RelayCommand ChangeNameClick { get; set; }
-        //public RelayCommand DeleteClick { get; set; }
+        public RelayCommand ChangeNameClick { get; set; }
+        public RelayCommand DeleteClick { get; set; }
         public RelayCommand AddClick { get; set; }
         public StoreEditViewModel(ObservableCollection<Store> stores)
         {
             Stores = stores; // set the property Courses (that is bound to the view) to be the collection we get passed from the other View
-            //ChangeNameClick = new RelayCommand(ChangeName);
-            //DeleteClick = new RelayCommand(DeleteStore);
+            ChangeNameClick = new RelayCommand(ChangeName);
+            DeleteClick = new RelayCommand(DeleteStore);
             AddClick = new RelayCommand(AddStore);
         }
 
@@ -35,15 +36,29 @@ namespace PreagusFietsen.ViewModel
             Stores.Add(data);
         }
 
-        //public void ChangeName(object a)
-        //{
-        
-        //}
+        public void ChangeName(object a)
+        {
+            if (SelectedStore != null)
+            {
+                SelectedStore.Address = "CHANGED ADDRESS";
+            }
+            else
+            {
+                MessageBox.Show("Please select a Store please");
+            }
+        }
 
-        //public void DeleteStore(object a)
-        //{
-
-        //}
+        public void DeleteStore(object a)
+        {
+            if (SelectedStore != null)
+            {
+                Stores.Remove(SelectedStore);
+            }
+            else
+            {
+                MessageBox.Show("Please select a Store please");
+            }
+        }
 
 
     }
